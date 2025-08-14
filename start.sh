285be80956418f6d4bc620dbd041afc5ee944059
GamerJay <dagamerjay13@gmail.com>
2025-08-14 03:42:48 -0400

    Revert: Restore parameterized start script

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+PORT=${PORT:-8080}
+WEB_CONCURRENCY=${WEB_CONCURRENCY:-1}
+WORKER_CLASS=${WORKER_CLASS:-sync}
+GUNICORN_TIMEOUT=${GUNICORN_TIMEOUT:-30}
 cd /app/backend
-exec gunicorn app:app --bind 0.0.0.0:8080 --workers 1 --timeout 30
+exec gunicorn app:app --bind 0.0.0.0:$PORT --workers $WEB_CONCURRENCY --worker-class $WORKER_CLASS --timeout $GUNICORN_TIMEOUT
